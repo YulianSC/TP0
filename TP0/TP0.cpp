1d4cbb938f1a1c22f0baa040640e6efeea39380e
YulianSC <jcopacarlos@frba.utn.edu.ar>
2026-04-27 22:53:42 -0300

    Se agrega la carpeta TP0

---
+++ TP0/TP0.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+//using namespace std;
+
+int main(){
+    std::cout<<"hola mundo\n";
+    std::cout<<"Nombre: Julian Copa \n";
+    std::cout<<"Curso: Algoritmos y EStructura de Datos K1053\n";
+    std::cout<<"Grupo: 4\n";
+
+}
